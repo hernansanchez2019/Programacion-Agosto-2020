cbb47897274ee488bd587c5e1fa3fb47bc37a096
comandos h <hernansanchez_91@hotmail.com.ar>
2020-09-09 23:31:01 -0300

    FUNCIONES VAIDACIONES

---
+++ CLASE 5/src/CLASE 5.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : CLASE.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+{
+	setbuf(stdout,NULL);
+	printf("CLASE 5"); /* prints !!!Hello World!!! */
+	return 0;
+}
