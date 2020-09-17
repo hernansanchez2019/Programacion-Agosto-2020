239f1a26e2459e4fb27ecd4c49c828afa69e7e4b
comandos h <hernansanchez_91@hotmail.com.ar>
2020-09-17 18:07:20 -0300

    ejercicios

--- CLASE 7 CODEBLOCKS/main.c
+++ CLASE 7 CODEBLOCKS/main.c
@@ -2,31 +2,58 @@
 #include <stdlib.h>
 #define EMPLEADOS 100
 
+
+void inicializar(int edad[], float sueldo[], int empleados);
+void cargarEmpleado(int edad[], float sueldo[], int empleados);
+void promedioSueldos(float sueldo[], int empleados);
+void mostrarEmpleados(int edad[], float sueldo[], int empleados);
+
+
 int main(void)
 
 {
-    //setbuf(stdout,NULL);
 
     int edades[EMPLEADOS];
-    int sueldo[EMPLEADOS];
-    int posicion;
-    char respuesta;
-    int i;
-    float promedio=0;
-    int suma=0;
-    int contadorPos=0;
+    float sueldo[EMPLEADOS];
 
     // inicializo
 
-    for(i=0 ; i <EMPLEADOS ; i++)
-    {
-        edades[i]=0;
-        sueldo[i]=0;
-    }
+    inicializar(edades, sueldo, EMPLEADOS);
 
 
     // pedimos datos
 
+    cargarEmpleado(edades,sueldo, EMPLEADOS);
+
+
+    // mostramos datos
+
+    mostrarEmpleados(edades, sueldo, EMPLEADOS);
+
+    // mostramos promedio
+
+    promedioSueldos(sueldo, EMPLEADOS);
+
+
+    return 0;
+}
+
+void inicializar(int edad[], float sueldo[], int empleados)
+{
+    int i;
+
+    for(i=0 ; i <empleados ; i++)
+    {
+        edad[i]=0;
+        sueldo[i]=0;
+    }
+
+}
+
+void cargarEmpleado(int edad[], float sueldo[], int empleados)
+{
+    int posicion;
+    char respuesta;
 
     do
     {
@@ -34,36 +61,53 @@ int main(void)
         scanf("%d", &posicion);
 
         printf("\nIngrese una edad: ");
-        scanf("%d", &edades[posicion]);
+        scanf("%d", &edad[posicion]);
 
         printf("Ingrese sueldo: ");
-        scanf("%d", &sueldo[posicion]);
+        scanf("%f", &sueldo[posicion]);
 
         printf("Desea cargar mas datos? s/n : ");
         fflush(stdin);
         scanf("%c", &respuesta);
-
-        suma= suma + sueldo[posicion];
-
-        contadorPos++;
-
     }
     while(respuesta=='s');
+}
+
+void mostrarEmpleados(int edad[], float sueldo[], int empleados)
+{
+    int i;
+
+    printf("\n\t*** Legajos Cargados ***\n");
+
+    for(i=0 ; i <empleados ; i++)
+    {
+
+        if(edad[i]!=0 && sueldo[i]!=0)
+        {
+            printf("\nLegajo= %d Edad= %d sueldo= %f\n",i, edad[i], sueldo[i]);
+        }
+    }
+}
+
+void promedioSueldos(float sueldo[], int empleados)
+{
+    int i;
+    int suma=0;
+    float promedio=0;
+    int contadorPos=0;
+
+    for(i= 0 ; i < empleados ; i++)
+    {
+        if(sueldo[i] > 0 )
+        {
+            suma= suma + sueldo[i];
+            contadorPos++;
+        }
+
+    }
 
     promedio= (float)suma / contadorPos;
 
+    printf("\nPromedio de sueldos: %f\n", promedio);
 
-    //	 mostramos datos
-
-    for(i=1 ; i <EMPLEADOS ; i++)
-    {
-
-        printf("Legajo= %d Edad= %d sueldo= %d\n", i, edades[i], sueldo[i]);
-    }
-
-    printf("Promedio de sueldos: %f", promedio);
-
-
-
-    return 0;
 }
